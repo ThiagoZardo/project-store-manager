cb15305d94d1ff2bb761971967d5434bd1947bad
ThiagoZardo <thiago.artevisual@gmail.com>
2022-07-02 23:15:44 -0300

    Até o 6 100% das funções testadas

--- tests/unit/services/salesServices.test.js
+++ tests/unit/services/salesServices.test.js
@@ -21,7 +21,7 @@ describe('100. Verifica função checkProductExists', () => {
   });
 });
 
-describe('Verifica a função registerSale salesService', () => {
+describe('101 Verifica a função registerSale salesService', () => {
   receip = [{ productId: 9999, quantity: 1 }];
 
   before(async () => {
@@ -34,6 +34,64 @@ describe('Verifica a função registerSale salesService', () => {
 
   it('Não permite cadastro de produtos que não existem', async () => {
     const response = await salesServices.registerSale(receip);
+    expect(response).to.equal(false);
+  });
+});
+
+describe('102. Verifica função registerSale', () => {
+  const sale = [{ productId: 1, quantity: 1 }, { productId: 2, quantity: 5 }];
+  returnRegisterSaleProduct = {
+    id: 3,
+    itemsSold: [{ productId: 1, quantity: 1 }, { productId: 2, quantity: 5 }]
+  }
+
+  before(async () => {
+    sinon.stub(salesServices, 'checkProductExists').resolves(false);
+    sinon.stub(salesModels, 'registerSaleProduct').resolves(returnRegisterSaleProduct);
+  });
+
+  after(async () => {
+    sinon.restore()
+  });
+
+  it('Verifica se retorna o id do produto cadastrado', async () => {
+    const response = await salesServices.registerSale(sale);
+    expect(response).to.equal(returnRegisterSaleProduct);
+  });
+});
+
+describe('103. Verifica função registerSale', () => {
+  const sale = [{ productId: 1, quantity: 1 }, { productId: 2, quantity: 5 }];
+  
+  before(async () => {
+    sinon.stub(salesServices, 'checkProductExists').resolves(false);
+    sinon.stub(salesModels, 'registerSaleProduct').resolves(false);
+  });
+
+  after(async () => {
+    sinon.restore()
+  });
+
+  it('Verifica se retorna o id do produto cadastrado', async () => {
+    const response = await salesServices.registerSale(sale);
+    expect(response).to.equal(false);
+  });
+});
+
+describe('102. Verifica função checkProductExists', () => {
+  const sale = [{ productId: 9999, quantity: 1 }];
+  const productExist = [[]];
+
+  before(async () => {
+    sinon.stub(salesModels, 'checkProductExists').resolves(productExist);
+  });
+
+  after(async () => {
+    sinon.restore()
+  });
+
+  it('Verifica se retorna o id do produto cadastrado', async () => {
+    const response = await salesServices.checkProductExists(sale);
     expect(response).to.equal(false);
   });
 });
